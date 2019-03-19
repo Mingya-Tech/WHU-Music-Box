54f6db12558414a85011321d559bd79b2d5a91fd
CasPaPaPa <zeonphantom@163.com>
2019-03-19 18:06:59 +0800

    分离音符定义和歌曲代码

---
+++ Beats.h
@@ -0,0 +1,6 @@
+// 列出所有节拍
+#define WHOLE 1
+#define HALF 0.5
+#define QUARTER 0.25
+#define EIGHTH 0.125
+#define SIXTEENTH 0.0625
---
+++ NTDs.h
@@ -0,0 +1,25 @@
+// 列出所有D调的频率
+#define NTD0 -1
+#define NTD1 294
+#define NTD2 330
+#define NTD3 350
+#define NTD4 393
+#define NTD5 441
+#define NTD6 495
+#define NTD7 556
+
+#define NTDL1 147
+#define NTDL2 165
+#define NTDL3 175
+#define NTDL4 196
+#define NTDL5 221
+#define NTDL6 248
+#define NTDL7 278
+
+#define NTDH1 589
+#define NTDH2 661
+#define NTDH3 700
+#define NTDH4 786
+#define NTDH5 882
+#define NTDH6 990
+#define NTDH7 1112
